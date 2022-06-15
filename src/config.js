6a9d945f8a71ca778e9386c275061629ba3be50a
0xVoider <nick@cryptobuilders.io>
2022-06-16 01:25:59 +0300

    adds pipedrive call to testing file

---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const PIPEDRIVE_TOKEN = 'eaa383cda8865ec5eff3be3c7d9ed982dddb4bca';
+export const GIT_TOKEN = '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';
--- src/test.js
+++ src/test.js
@@ -1,6 +1,15 @@
+const pipedrive = require('pipedrive');
+
+const client = pipedrive.ApiClient.instance;
+// let apiToken = client.authentications.api_Key;
+  client.authentications.api_key.apiKey = 'eaa383cda8865ec5eff3be3c7d9ed982dddb4bca';
+
   const user = '0xvoider42';
   const apiCall = 'gists';
   const gistStore = [];
+  const gistNames = [];
+  const url = `https://api.pipedrive.com/v1/globalMessages?api_token=eaa383cda8865ec5eff3be3c7d9ed982dddb4bca`
+
 
   const gitCall = async () => {
     const response = await fetch(`https://api.github.com/users/${user}/${apiCall}`);
@@ -9,7 +18,41 @@
     for (const key in gists) {
       gistStore.push(gists[key].files);
     }
-    console.log(gistStore)
+
+    gistStore.forEach(gist => {
+      for(let key in gist) {
+        gistNames.push(gist[key].filename);
+      }
+    })
+    gistNames.map(name => console.log(name))
   }
 
-  gitCall()
+  async function addActivity() {
+    try{
+      const api = new pipedrive.ActivitiesApi();
+      const response = await api.addActivity({
+        subject: 'test for the tests sake',
+        type: 'football'
+      });
+      console.log(response)
+    } catch(err) {
+      const errorToLog = err.context?.body || err;
+      console.log(errorToLog)
+    }
+  }
+
+  addActivity();
+
+// async function postGists(url = '', data = []) {
+//     const response = await fetch(url, { 
+//       method: 'POST',
+//       headers: {
+//       'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(data)
+//     })
+//     return response.json()
+//   }
+
+// postGists(url, test).then(data =>
+//     {console.log(data)})
