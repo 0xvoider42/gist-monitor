1fa00d4f0288dd38979204dc40369e951d32274e
0xVoider <nick@cryptobuilders.io>
2022-06-16 01:25:02 +0300

    adds call to gitAPI and lists gists

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import React from 'react'
+import React, {useState} from 'react'
+import { PIPEDRIVE_TOKEN } from './config.js'
 
 function App() {
 
+  const [names, setName] = useState([]);
   const user = '0xvoider42';
   const apiCall = 'gists';
   const gistStore = [];
+  const gistNames = [];
 
   const gitCall = async () => {
     const response = await fetch(`https://api.github.com/users/${user}/${apiCall}`);
@@ -14,12 +17,27 @@ function App() {
     for (const key in gists) {
       gistStore.push(gists[key].files);
     }
-    console.log(gistStore)
+
+    gistStore.forEach(gist => {
+      for(let key in gist) {
+        gistNames.push(gist[key].filename);
+      }
+    })
+    setName(gistNames)
+  }
+
+  const pipeDriveCall = async () => {
+    const response = await fetch(`https://api.pipedrive.com/v1/globalMessages?api_token=${PIPEDRIVE_TOKEN}`)
+    const data = await response.json();
+    console.log(data)
   }
 
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          {names.map(name => <li>{name}</li>)}
+        </div>
         <button className="getGists" onClick={gitCall}>Gists</button>
       </header>
     </div>
