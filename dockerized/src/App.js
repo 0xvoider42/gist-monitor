cec03ae94f2fa3cf004e85bdd7b3ab37f2c1b817
0xVoider <nick@cryptobuilders.io>
2022-06-16 18:02:16 +0300

    adds actions

--- dockerized/src/App.js
+++ dockerized/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { gitCall } from '../../CreateActivity'
+import { gitCall } from './CreateActivity'
 
 function App() {
   const Timer = () => {
