d81b791ca9475c31f91aaaacfce3cc91557cef34
0xVoider <nick@cryptobuilders.io>
2022-06-19 17:45:41 +0300

    adds calls to git and pipedrive apis, stores gists to pipedrive activities

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const pipedrive = require('pipedrive');
+const client = pipedrive.ApiClient.instance;
+client.authentications.api_key.apiKey = 'your pipedrive key'
+
 const GIT_USER = '0xvoider42';
 let arrayCheck;
 let storage = [];
+let nameArr = []
+let descriptionArr = []
+let urlArr = []
 
 const gitCall = async() => {
   const response = await fetch(`https://api.github.com/users/${GIT_USER}/gists`);
@@ -26,9 +33,45 @@ const gitCall = async() => {
       arrayCheck = data.length;
     }
   }
-  console.log('FILES', storage);
+
+  nameArr.length = 0;
+  storage.forEach(element => nameArr.push(Object.keys(element)[0]));
+  descriptionArr.length = 0;
+  data.forEach(element => descriptionArr.push(element.description));
+  urlArr.length = 0;
+  data.forEach(element => urlArr.push(element.html_url));
+
+  console.log('Name: ', nameArr,'Description', descriptionArr,'URL', urlArr);
+
+  if(nameArr.length != 0){
+    for(let i = 0; i < nameArr.length; i++){
+      addActivity(nameArr[i], descriptionArr[i], urlArr[i]);
+      console.log('Activities have been created with the gists: ', nameArr.length)
+    }
+  } else if (nameArr.length == 0) {
+    console.log('Waiting to refresh the storage...');
+  } else {
+    console.log('something went wrong...', error);
+  }
+
+  return console.log('Latest gists are:', nameArr, descriptionArr, urlArr)
 }
 
+  const addActivity = async (subject, description, url) => {
+    try{
+      const api = new pipedrive.ActivitiesApi();
+      const response = await api.addActivity({
+        subject: subject,
+        type: 'task',
+        note: description,
+        leadId: url
+      });
+      console.log(response)
+    } catch(err) {
+      const errorToLog = err.context?.body || err;
+      console.log(errorToLog)
+    }
+  }
 
-setInterval(gitCall, 15 * 1000)
 
+setInterval(gitCall, 20 * 1000)
